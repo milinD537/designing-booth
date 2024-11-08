cfe89c3e41112f3f9582f83d800361043423f7b3
rahulsingh2312 <rahulsinghhh2312@gmail.com>
2024-11-08 18:36:20 +0530

    might break

---
+++ src/app/services/page.tsx
@@ -0,0 +1,19 @@
+import Services from "@/components/services"
+
+export default function services() {
+    return (
+        <div>
+<section className="servicesWrapper | mt-28 lg:px-12">
+				<Services />
+                <iframe
+  width="100%"
+  // style={{ backgroundColor: "#5D5BB9]" }}
+  className="h-[1150px] lg:rounded-3xl md:mt-10  md:h-[700px] bg-[url(/images/noise.webp),linear-gradient(to_bottom,#2B2A57,#2B2A57_65%)]"
+  src="https://bento.me/octoreach"
+  title="description"
+  scrolling="no"
+/>
+                </section>
+        </div>
+    )
+}
